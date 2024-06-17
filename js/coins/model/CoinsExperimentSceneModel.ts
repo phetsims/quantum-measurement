d751c8ed167365cdf592ad4a1c6af9a4bb6606ad
jbphet <jpblanco@yahoo.com>
2024-06-17 13:14:18 -0600

    set bias for quantum scene when initial state is changed

--- js/coins/model/CoinsExperimentSceneModel.ts
+++ js/coins/model/CoinsExperimentSceneModel.ts
@@ -113,6 +113,13 @@ export default class CoinsExperimentSceneModel extends PhetioObject {
         this.singleCoin.currentStateProperty.value = this.initialCoinStateProperty.value;
       }
     } );
+
+    // If this is a quantum system, changing the initial state of the coin sets the bias to match that coin.
+    if ( this.systemType === 'quantum' ) {
+      this.initialCoinStateProperty.link( initialCoinState => {
+        this.stateBiasProperty.value = initialCoinState === 'up' ? 0 : 1;
+      } );
+    }
   }
 
   public reset(): void {
