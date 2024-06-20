26967c31c8681368f4407f90432decf89423befa
jbphet <jpblanco@yahoo.com>
2024-06-20 16:03:08 -0600

    add mockup back t 1st screen after publishing dev version

--- js/coins/view/CoinsScreenView.ts
+++ js/coins/view/CoinsScreenView.ts
@@ -8,9 +8,11 @@
 
 import quantumMeasurement from '../../quantumMeasurement.js';
 import CoinsModel from 'model/CoinsModel.js';
+import coinsScreenMockup_png from '../../../images/coinsScreenMockup_png.js';
 import QuantumMeasurementScreenView from '../../common/view/QuantumMeasurementScreenView.js';
 import Tandem from '../../../../tandem/js/Tandem.js';
-import { Color, Text } from '../../../../scenery/js/imports.js';
+import { Color, Image, Text } from '../../../../scenery/js/imports.js';
+import ScreenView from '../../../../joist/js/ScreenView.js';
 import Vector2 from '../../../../dot/js/Vector2.js';
 import RectangularRadioButtonGroup from '../../../../sun/js/buttons/RectangularRadioButtonGroup.js';
 import { SystemType, SystemTypeValues } from '../../common/model/SystemType.js';
@@ -39,9 +41,9 @@ export default class CoinsScreenView extends QuantumMeasurementScreenView {
   public constructor( model: CoinsModel, tandem: Tandem ) {
 
     super( {
-      // mockupImage: new Image( coinsScreenMockup_png, {
-      //   scale: ScreenView.DEFAULT_LAYOUT_BOUNDS.width / coinsScreenMockup_png.width
-      // } ),
+      mockupImage: new Image( coinsScreenMockup_png, {
+        scale: ScreenView.DEFAULT_LAYOUT_BOUNDS.width / coinsScreenMockup_png.width
+      } ),
       initialMockupOpacity: 0,
       tandem: tandem
     } );
