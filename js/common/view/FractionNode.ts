f95498d08a2f602c98bbe23a3ce41f2e1ca15988
AgustinVallejo <agustin.vallejo@udea.edu.co>
2024-10-23 11:23:46 -0500

    Adding a common FractionNode

---
+++ js/common/view/FractionNode.ts
@@ -0,0 +1,45 @@
+// Copyright 2023, University of Colorado Boulder
+/**
+ * Node that displays a fraction that grows dynamically based on the contents.
+ *
+ * @author Agustín Vallejo
+ */
+
+import quantumMeasurement from '../../quantumMeasurement.js';
+import { Line, Node, NodeOptions, VBoxOptions } from '../../../../scenery/js/imports.js';
+import Multilink from '../../../../axon/js/Multilink.js';
+import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
+import StrictOmit from '../../../../phet-core/js/types/StrictOmit.js';
+
+const FRACTION_LINE_MARGIN = 5;
+
+type SelfOptions = EmptySelfOptions;
+
+type FractionNodeOptions = SelfOptions & StrictOmit<VBoxOptions, 'children'>;
+
+export default class FractionNode extends Node {
+  public constructor( numeratorNode: Node, denominatorNode: Node, providedOptions?: FractionNodeOptions ) {
+
+    const fractionLine = new Line( 0, 0, 1, 0, { stroke: 'black', lineWidth: 1.5, lineCap: 'round' } );
+
+    const options = optionize<FractionNodeOptions, SelfOptions, NodeOptions>()( {
+      children: [
+        numeratorNode,
+        fractionLine,
+        denominatorNode
+      ]
+    }, providedOptions );
+
+    super( options );
+
+    // Dynamically size the fraction line.
+    Multilink.multilink( [ numeratorNode.boundsProperty, denominatorNode.boundsProperty ],
+      ( numeratorBounds, denominatorBounds ) => {
+        fractionLine.setLine( 0, 0, Math.max( numeratorBounds.width, denominatorBounds.width ) + FRACTION_LINE_MARGIN, 0 );
+        numeratorNode.centerBottom = fractionLine.centerTop.minusXY( 0, 5 );
+        denominatorNode.centerTop = fractionLine.centerBottom.plusXY( 0, 5 );
+      } );
+  }
+}
+
+quantumMeasurement.register( 'FractionNode', FractionNode );
