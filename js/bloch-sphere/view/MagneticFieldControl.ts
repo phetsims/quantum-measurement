ed4589a85b1f7060e20cac28ef7d041358d41e9d
jbphet <jpblanco@yahoo.com>
2025-01-02 17:37:41 -0700

    refine magnetic field control, see https://github.com/phetsims/quantum-measurement/issues/54

--- js/bloch-sphere/view/MagneticFieldControl.ts
+++ js/bloch-sphere/view/MagneticFieldControl.ts
@@ -8,12 +8,14 @@
  */
 
 import NumberProperty from '../../../../axon/js/NumberProperty.js';
+import Dimension2 from '../../../../dot/js/Dimension2.js';
+import Utils from '../../../../dot/js/Utils.js';
 import { Shape } from '../../../../kite/js/imports.js';
 import optionize, { EmptySelfOptions } from '../../../../phet-core/js/optionize.js';
 import Orientation from '../../../../phet-core/js/Orientation.js';
 import WithRequired from '../../../../phet-core/js/types/WithRequired.js';
 import PhetFont from '../../../../scenery-phet/js/PhetFont.js';
-import { HBox, Node, Path, Text, VBox } from '../../../../scenery/js/imports.js';
+import { Color, HBox, Node, Path, Text, VBox } from '../../../../scenery/js/imports.js';
 import Panel, { PanelOptions } from '../../../../sun/js/Panel.js';
 import Slider from '../../../../sun/js/Slider.js';
 import QuantumMeasurementColors from '../../common/QuantumMeasurementColors.js';
@@ -23,6 +25,8 @@ import MagneticFieldArrowNode from './MagneticFieldArrowNode.js';
 
 type SelfOptions = EmptySelfOptions;
 export type MagneticFieldControlOptions = SelfOptions & WithRequired<PanelOptions, 'tandem'>;
+
+const SLIDER_TRACK_SIZE = new Dimension2( 0.1, 100 );
 
 export default class MagneticFieldControl extends Panel {
 
@@ -36,29 +40,37 @@ export default class MagneticFieldControl extends Panel {
       yMargin: 10
     }, providedOptions );
 
-    const indicatorAndSlider = new HBox( {
+    const magneticFieldIndicator = new Node( {
       children: [
-        new Node( {
-          centerX: 0,
-          centerY: 0,
-          children: [
-            new Path( new Shape().moveTo( 0, -50 ).lineTo( 0, 50 ), {
-              stroke: 'grey',
-              lineWidth: 1,
-              lineDash: [ 5, 5 ]
-            } ),
-            new MagneticFieldArrowNode( magneticFieldStrengthProperty )
-          ]
+        new Path( new Shape().moveTo( 0, -SLIDER_TRACK_SIZE.height / 2 ).lineTo( 0, SLIDER_TRACK_SIZE.height / 2 ), {
+          stroke: 'grey',
+          lineWidth: 1,
+          lineDash: [ 5, 5 ]
         } ),
-        new Slider( magneticFieldStrengthProperty, magneticFieldStrengthProperty.range, {
-          tandem: providedOptions.tandem.createTandem( 'magneticFieldStrengthSlider' ),
-          thumbFill: '#ff0',
-          orientation: Orientation.VERTICAL,
-          centerX: 50,
-          centerY: 0
-        } )
+        new MagneticFieldArrowNode( magneticFieldStrengthProperty )
       ],
-      spacing: 10
+      tandem: providedOptions.tandem.createTandem( 'magneticFieldIndicator' )
+    } );
+
+    const magneticFieldStrengthSlider = new Slider( magneticFieldStrengthProperty, magneticFieldStrengthProperty.range, {
+      tandem: providedOptions.tandem.createTandem( 'magneticFieldStrengthSlider' ),
+      thumbSize: new Dimension2( 28, 14 ),
+      thumbFill: '#ee0',
+      thumbFillHighlighted: '#ff0',
+      thumbCenterLineStroke: Color.BLACK,
+      trackSize: SLIDER_TRACK_SIZE,
+      trackFillEnabled: Color.BLACK,
+      constrainValue: value => Utils.roundToInterval( value, 0.25 ),
+      orientation: Orientation.VERTICAL,
+      majorTickLength: 20
+    } );
+    magneticFieldStrengthSlider.addMajorTick( -1 );
+    magneticFieldStrengthSlider.addMajorTick( 1 );
+
+    const indicatorAndSlider = new HBox( {
+      children: [ magneticFieldIndicator, magneticFieldStrengthSlider ],
+      spacing: 20,
+      resize: false
     } );
 
     const panelTitle = new Text( QuantumMeasurementStrings.magneticFieldStringProperty, {
@@ -68,7 +80,7 @@ export default class MagneticFieldControl extends Panel {
     super(
       new VBox( {
         children: [ panelTitle, indicatorAndSlider ],
-        spacing: 20
+        spacing: 10
       } ),
       options
     );
