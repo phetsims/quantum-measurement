d9d7c2ace7b8bc7949b8cda29685f6712fee14b4
phet-dev <phetadmin@gmail.com>
2025-01-05 03:41:12 -0700

    update copyright dates from daily grunt work

--- js/bloch-sphere/model/StateDirection.ts
+++ js/bloch-sphere/model/StateDirection.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * Represents the possible preset directions the state vector within the Bloch Sphere can have.
