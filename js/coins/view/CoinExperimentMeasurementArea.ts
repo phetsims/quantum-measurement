08543ace8a86f0b8d6cc90f945d8990cbe2cb121
phet-dev <phetadmin@gmail.com>
2025-01-08 03:44:43 -0700

    update copyright dates from daily grunt work

--- js/coins/view/CoinExperimentMeasurementArea.ts
+++ js/coins/view/CoinExperimentMeasurementArea.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * CoinExperimentMeasurementArea is a composite UI component that presents two areas - one for a single coin and one for
