34253ffc59de97d9c3f0ee8e25d9aaf60db5578f
phet-dev <phetadmin@gmail.com>
2025-01-09 03:55:22 -0700

    update copyright dates from daily grunt work

--- js/spin/view/BlochSphereWithProjectionNode.ts
+++ js/spin/view/BlochSphereWithProjectionNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * BlochSphereWithProjectionNode is an extension of BlochSphereNode with vector projections on X and Z.
