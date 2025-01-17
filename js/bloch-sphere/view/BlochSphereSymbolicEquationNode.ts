551fa734be4d2e92b96e19448611022ade2aa403
phet-dev <phetadmin@gmail.com>
2025-01-17 03:29:17 -0700

    update copyright dates from daily grunt work

--- js/bloch-sphere/view/BlochSphereSymbolicEquationNode.ts
+++ js/bloch-sphere/view/BlochSphereSymbolicEquationNode.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * BlochSphereSymbolicEquationNode displays the equation that is used to calculate the expected value of polarization.
