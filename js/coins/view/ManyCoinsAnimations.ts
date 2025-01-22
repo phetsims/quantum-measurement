c9a9fe9506208de574b0453f1af002297f0e805b
phet-dev <phetadmin@gmail.com>
2025-01-22 03:38:42 -0700

    update copyright dates from daily grunt work

--- js/coins/view/ManyCoinsAnimations.ts
+++ js/coins/view/ManyCoinsAnimations.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
  * ManyCoinsAnimations handles the exceptional 10_000 coins case, where each coin is represented as a pixel.
--- js/spin/view/SpinStatePreparationArea.ts
+++ js/spin/view/SpinStatePreparationArea.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * SpinStatePreparationArea is a composite UI component that allows users to prepare the spin state of a quantum system
