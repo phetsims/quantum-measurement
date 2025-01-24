e62975843d552956550287d08a8d85cda4a0ac22
phet-dev <phetadmin@gmail.com>
2025-01-24 04:02:56 -0700

    update copyright dates from daily grunt work

--- js/common/model/AbstractBlochSphere.ts
+++ js/common/model/AbstractBlochSphere.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * AbstractBlochSphere is a simple model for a sphere representation of a Quantum system.
--- js/spin/model/SpinDirection.ts
+++ js/spin/model/SpinDirection.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * SpinDirection contains information of the three supported spin states in the preparation area.
