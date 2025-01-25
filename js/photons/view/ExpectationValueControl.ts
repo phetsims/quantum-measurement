9c5d0a03b1503d6fc57a4f74ac473744bdbfc1ee
phet-dev <phetadmin@gmail.com>
2025-01-25 03:35:26 -0700

    update copyright dates from daily grunt work

--- js/photons/view/ExpectationValueControl.ts
+++ js/photons/view/ExpectationValueControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2024-2025, University of Colorado Boulder
 
 /**
  * ExpectationValueControl is a view element that allows the user to control the visibility of the expectation value
--- js/photons/view/ExpectationValueVectorControl.ts
+++ js/photons/view/ExpectationValueVectorControl.ts
@@ -1,4 +1,4 @@
-// Copyright 2024, University of Colorado Boulder
+// Copyright 2025, University of Colorado Boulder
 
 /**
  * ExpectationValueVectorControl is a view element that allows the user to control the visibility of the expectation
