1dc361c607e20e09d963a87496890a2570d4c34a
jbphet <jpblanco@yahoo.com>
2025-02-25 16:41:13 -0700

    improve button a11y behavior, see https://github.com/phetsims/quantum-measurement/issues/128

--- js/photons/view/LaserNode.ts
+++ js/photons/view/LaserNode.ts
@@ -21,6 +21,7 @@ import AlignBox from '../../../../scenery/js/layout/nodes/AlignBox.js';
 import Node, { NodeOptions } from '../../../../scenery/js/nodes/Node.js';
 import Text from '../../../../scenery/js/nodes/Text.js';
 import Color from '../../../../scenery/js/util/Color.js';
+import RoundPushButton from '../../../../sun/js/buttons/RoundPushButton.js';
 import HSlider from '../../../../sun/js/HSlider.js';
 import QuantumMeasurementColors from '../../common/QuantumMeasurementColors.js';
 import quantumMeasurement from '../../quantumMeasurement.js';
@@ -30,35 +31,37 @@ import Laser from '../model/Laser.js';
 type SelfOptions = EmptySelfOptions;
 type LaserNodeOptions = SelfOptions & PickRequired<NodeOptions, 'tandem'>;
 
+// constants
+const LASER_BODY_SIZE = new Dimension2( 95, 55 );
+const NOZZLE_SIZE = new Dimension2( 15, 45 );
+
 export default class LaserNode extends Node {
 
   public constructor( model: Laser,
                       modelViewTransform: ModelViewTransform2,
                       providedOptions: LaserNodeOptions ) {
 
-    // The button on the laser is only used in single-photon mode, but the property for it has to be created and passed
-    // in to the node regardless.
-    const buttonOnProperty = new BooleanProperty( false );
+    // If in single-photon mode, we need a button to emit a photon.
+    const laserPointerNodeChildren: Node[] = [];
     if ( model.emissionMode === 'singlePhoton' ) {
-      buttonOnProperty.link( buttonOn => {
-        if ( buttonOn ) {
-          model.emitAPhoton();
-        }
-      } );
+      laserPointerNodeChildren.push( new RoundPushButton( {
+        baseColor: 'red',
+        radius: 18,
+        fireOnDown: true,
+        listener: () => model.emitAPhoton(),
+        centerX: -( NOZZLE_SIZE.width + LASER_BODY_SIZE.width / 2 ),
+        touchAreaDilation: 15
+      } ) );
     }
 
-    // Create the laser pointer node using a common code component.
-    const laserBodySize = new Dimension2( 95, 55 );
-    const nozzleSize = new Dimension2( 15, 45 );
-    const laserPointerNode = new LaserPointerNode( buttonOnProperty, {
-      bodySize: laserBodySize,
-      nozzleSize: nozzleSize,
-      hasButton: model.emissionMode === 'singlePhoton',
-      buttonType: 'momentary',
-      buttonOptions: {
-        radius: 18
-      },
-      right: modelViewTransform.modelToViewX( model.position.x ) + nozzleSize.width,
+    // Create the laser pointer node.
+    const dummyProperty = new BooleanProperty( false ); // required by LaserPointerNode
+    const laserPointerNode = new LaserPointerNode( dummyProperty, {
+      bodySize: LASER_BODY_SIZE,
+      nozzleSize: NOZZLE_SIZE,
+      hasButton: false, // Due to this sim's specific needs we have to supply our own button.
+      children: laserPointerNodeChildren,
+      right: modelViewTransform.modelToViewX( model.position.x ) + NOZZLE_SIZE.width,
       centerY: modelViewTransform.modelToViewY( model.position.y ),
       tandem: providedOptions.tandem.createTandem( 'laserPointerNode' ),
       phetioVisiblePropertyInstrumented: false
@@ -79,13 +82,13 @@ export default class LaserNode extends Node {
     // the rate.
     if ( model.emissionMode === 'manyPhotons' ) {
       const emissionRateSlider = new HSlider( model.emissionRateProperty, model.emissionRateProperty.range, {
-        trackSize: new Dimension2( laserBodySize.width * 0.67, 2 ),
+        trackSize: new Dimension2( LASER_BODY_SIZE.width * 0.67, 2 ),
         trackStroke: Color.DARK_GRAY,
         trackFillEnabled: Color.BLACK,
-        thumbSize: new Dimension2( laserBodySize.height * 0.25, laserBodySize.height * 0.5 ),
+        thumbSize: new Dimension2( LASER_BODY_SIZE.height * 0.25, LASER_BODY_SIZE.height * 0.5 ),
         thumbFill: QuantumMeasurementColors.photonBaseColorProperty,
         thumbFillHighlighted: 'rgb( 0, 200, 0)',
-        center: laserPointerNode.bounds.center.plusXY( -nozzleSize.width / 2, 0 ),
+        center: laserPointerNode.bounds.center.plusXY( -NOZZLE_SIZE.width / 2, 0 ),
         constrainValue: value => roundSymmetric( value ),
         tandem: providedOptions.tandem.createTandem( 'emissionRateSlider' )
       } );
