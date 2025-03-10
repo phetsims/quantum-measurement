8f67a859f93b54e27a3c67d3a2a29aade224ca34
jbphet <jpblanco@yahoo.com>
2025-03-10 15:48:22 -0600

    add initial version of screen and nav bar icons for Photons screen, see https://github.com/phetsims/quantum-measurement/issues/88

--- js/photons/PhotonsScreen.ts
+++ js/photons/PhotonsScreen.ts
@@ -6,9 +6,16 @@
  * @author John Blanco, PhET Interactive Simulations
  */
 
-import ScreenIcon from '../../../joist/js/ScreenIcon.js';
+import Vector2 from '../../../dot/js/Vector2.js';
+import ScreenIcon, { MINIMUM_HOME_SCREEN_ICON_SIZE } from '../../../joist/js/ScreenIcon.js';
+import Shape from '../../../kite/js/Shape.js';
+import Image from '../../../scenery/js/nodes/Image.js';
+import Line from '../../../scenery/js/nodes/Line.js';
+import Node from '../../../scenery/js/nodes/Node.js';
 import Rectangle from '../../../scenery/js/nodes/Rectangle.js';
 import Tandem from '../../../tandem/js/Tandem.js';
+import greenPhoton_png from '../../images/greenPhoton_png.js';
+import greenPhotonOutline_png from '../../images/greenPhotonOutline_png.js';
 import QuantumMeasurementColors from '../common/QuantumMeasurementColors.js';
 import QuantumMeasurementKeyboardHelpContent from '../common/view/QuantumMeasurementKeyboardHelpContent.js';
 import QuantumMeasurementScreen from '../common/view/QuantumMeasurementScreen.js';
@@ -22,7 +29,7 @@ export default class PhotonsScreen extends QuantumMeasurementScreen<PhotonsModel
   public constructor( tandem: Tandem ) {
 
     const options = {
-        name: QuantumMeasurementStrings.screen.photonsStringProperty,
+      name: QuantumMeasurementStrings.screen.photonsStringProperty,
       homeScreenIcon: createScreenIcon(),
       createKeyboardHelpNode: () => new QuantumMeasurementKeyboardHelpContent( { includeTimeControlsKeyboardHelp: true } ),
       // TODO: Fill this in with the real help text, see https://github.com/phetsims/quantum-measurement/issues/124
@@ -42,13 +49,77 @@ export default class PhotonsScreen extends QuantumMeasurementScreen<PhotonsModel
   }
 }
 
+/**
+ * A standalone photon (doesn't need a model) for use in the screen icon.
+ */
+class StandalonePhotonNode extends Node {
+
+  public static readonly RADIUS = 35;
+
+  public constructor( center: Vector2 ) {
+    const interiorNode = new Image( greenPhoton_png );
+    const exteriorNode = new Image( greenPhotonOutline_png );
+    super( {
+      children: [ interiorNode, exteriorNode ],
+      scale: StandalonePhotonNode.RADIUS * 2 / interiorNode.width,
+      center: center
+    } );
+  }
+}
+
+/**
+ * A standalone beam splitter (doesn't need a model) for use in the screen icon.
+ */
+class StandaloneBeamSplitterNode extends Node {
+
+  public static readonly LENGTH = 200;
+
+  public constructor( center: Vector2 ) {
+    const enclosure = new Rectangle( 0, 0, StandaloneBeamSplitterNode.LENGTH, StandaloneBeamSplitterNode.LENGTH, {
+      fill: QuantumMeasurementColors.splitterEnclosureNodeFillColorProperty
+    } );
+    const splitterLine = new Line( 0, StandaloneBeamSplitterNode.LENGTH, StandaloneBeamSplitterNode.LENGTH, 0, {
+      stroke: QuantumMeasurementColors.splitterLineNodeFillColorProperty,
+      lineWidth: 10,
+      lineCap: 'butt'
+    } );
+    super( {
+      children: [ enclosure, splitterLine ],
+      center: center,
+      opacity: 0.75
+    } );
+  }
+}
+
 const createScreenIcon = (): ScreenIcon => {
 
-  // TODO: Fill this in with the real deal, see https://github.com/phetsims/quantum-measurement/issues/88.
-  const iconNode = new Rectangle( 1, 1, 100, 100, { fill: QuantumMeasurementColors.photonBaseColorProperty } );
+  const size = MINIMUM_HOME_SCREEN_ICON_SIZE;
+
+  const background = new Rectangle( {
+      rectSize: size,
+      fill: QuantumMeasurementColors.quantumBackgroundColorProperty
+    }
+  );
+
+  const iconNode = new Node( {
+    children: [
+      background,
+      new StandaloneBeamSplitterNode( new Vector2( size.width / 2, size.height / 2 ) ),
+
+      // Photons, positioned around the beam splitter based on the spec, see #88.
+      new StandalonePhotonNode( new Vector2( size.width * 0.1, size.height / 2 ) ),
+      new StandalonePhotonNode( new Vector2( size.width * 0.3, size.height / 2 ) ),
+      new StandalonePhotonNode( new Vector2( size.width / 2, size.height * 0.3 ) ),
+      new StandalonePhotonNode( new Vector2( size.width / 2, 0 ) ),
+      new StandalonePhotonNode( new Vector2( size.width * 0.7, size.height / 2 ) ),
+      new StandalonePhotonNode( new Vector2( size.width * 0.9, size.height / 2 ) )
+    ],
+    clipArea: Shape.bounds( background.bounds )
+  } );
+
   return new ScreenIcon( iconNode, {
     maxIconWidthProportion: 1,
-    maxIconHeightProportion: 0.85,
+    maxIconHeightProportion: 1,
     fill: QuantumMeasurementColors.quantumBackgroundColorProperty
   } );
 };
